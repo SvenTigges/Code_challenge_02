09de1d5b15621eaf20d437690edd34c4c8cd697e
SvenTigges <sven.tigges@web.de>
2021-05-19 13:44:53 +0200

    Done

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,29 @@ Alter 13-17 // Cola
 */
 
 /* Getränke-Challenge */
+let firstName, age;
+firstName = prompt("Bitte den Namen eingeben:");
+age = prompt("Bitte das Alter eingeben:");
+
+switch (true) {
+    case (age>=0&&age<=5):
+        console.log(firstName + "ist " + age + "und darf somit Milch trinken");
+        break;
+    case (age>=6&&age<=12):
+        console.log(firstName + "ist " + age + "und darf somit Soft trinken");
+        break;
+    case (age>=13&&age<=17):
+        console.log(firstName + "ist " + age + "und darf somit Cola trinken");
+        break;
+    case (age>=18):
+        console.log(firstName + "ist " + age + "und darf somit Wein trinken");
+        break;
+    default:
+        console.log("error");
+        break;
+}
+
+
+
+
+
